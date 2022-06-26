9c373f7ab2aebae33065958cda2e681b00d65858
Nikita <zharinov.nicita@gmail.com>
2022-06-26 16:25:52 +0700

    [feature/module-artist] #22

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { ArtistModule } from './artist/artist.module'
 
 @Module({
-    imports: [MongooseModule.forRoot('mongodb://localhost:27017/api-vinevi')],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/api-vinevi'),
+    ArtistModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}

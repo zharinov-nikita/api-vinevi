16edc12edfd02b7e04735b1193f93ac0ddbb6d49
Nikita <zharinov.nicita@gmail.com>
2022-06-27 11:02:15 +0700

    [feature/controller-track] #56

---
+++ src/track/track.controller.ts
@@ -0,0 +1,24 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common'
+import { ObjectId } from 'mongoose'
+import { CreateTrackDto } from './dto/create-track.dto'
+import { UpdateTrackDto } from './dto/update-track.dto'
+import { Track } from './track.schema'
+import { TrackService } from './track.service'
+
+@Controller('api/track')
+export class TrackController {
+  constructor(private trackService: TrackService) {}
+
+  @Post()
+  async create(@Body() dto: CreateTrackDto): Promise<Track> {
+    return await this.trackService.create(dto)
+  }
+}

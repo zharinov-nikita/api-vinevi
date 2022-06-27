2afcab2da9c4f893b759ef21f58afd1647f2472f
Nikita <zharinov.nicita@gmail.com>
2022-06-27 10:37:36 +0700

    [feature/schema-trakc] #46

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { AlbumModule } from './album/album.module'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { ArtistModule } from './artist/artist.module'
 
 @Module({
@@ -11,7 +9,5 @@ import { ArtistModule } from './artist/artist.module'
     ArtistModule,
     AlbumModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
